91464f949771cf86b0d01c7dcbc82a0a95ff05b9
Isabela Blucher <isabelablucher@hotmail.com>
2016-10-27 13:34:25 -0200

    Funciona, mas sem lidar com bordas

--- decompress.m
+++ decompress.m
@@ -1,32 +1,32 @@
 function decompress(compressedImg, method, k, h)
   img = imread(compressedImg);
   [a, l, p] = size(img);
-  
+  a
   if method == 1 %bilinear
 
     n = a + (a-1)*k;
+    n
     D = expandMatrix(img, n, k);
 
-    for i=1:h:(a-h)
-      for j=1:h:(a-h)
+    for color=1:3
+      for i=1:h:(n-h)
+        for j=1:h:(n-h)
         %calcular pontos dados das bordas
-        i
-        j
-        fq00 = img(i, j, :);
-        fq10 = img(i+h, j, :);
-        fq01 = img(i, j+h, :);
-        fq11 = img(i+h, j+h, :); 
-    
+        fq00 = D(i, j, color);
+        fq10 = D(i+h, j, color);
+        fq01 = D(i, j+h, color);
+        fq11 = D(i+h, j+h, color);
+         
         A = [1, 0, 0, 0; 1, 0, h, 0; 1, h, 0, 0; 1, h, h, h.^2];
+        A_inv = inv(A);
 
         %preencher os pixels criados com a expansao do quadrado
-        for s = 1:3
+        B = [fq00; fq01; fq10; fq11];
+        B = double(B);
+        X = A_inv*B;
           for p=i:(i+h)
             for r=j:(j+h)
-              B = [fq00(s); fq01(s); fq10(s); fq11(s)];
-              B = double(B);
-              X = linsolve(A, B);
-              D(p,r) = X(1,1) + X(2,1)*(p - i) + X(3,1)*(r - j) + X(4,1)*(p - i)*(r - j);
+              D(p,r, color) = X(1,1) + X(2,1)*(p - i) + X(3,1)*(r - j) + X(4,1)*(p - i)*(r - j);
             endfor
           endfor
         endfor
@@ -37,12 +37,13 @@ function decompress(compressedImg, method, k, h)
 endfunction
 
 function D = expandMatrix(img, n, k) 
-  for i=1:(n-1)
-    for j=1:(n-1)
+  for i=1:(n)
+    for j=1:(n)
       if(rem(i, k+1) == 1 && rem(j,k+1) == 1)
         D(i,j,:) = img((i-1)/(k+1) + 1, (j-1)/(k+1) + 1, :);
       endif
     endfor
   endfor
+  size(D)(1)
 endfunction
        
